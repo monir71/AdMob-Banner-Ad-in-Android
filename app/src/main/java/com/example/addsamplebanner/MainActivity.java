4b0f028eb579f855cfb4c53a540685ae1ca11df7
monir71 <zamanmonir71@yahoo.com>
2025-07-11 21:42:55 +0600

    Banner Ad

---
+++ app/src/main/java/com/example/addsamplebanner/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.addsamplebanner;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+    AdView adView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        adView = findViewById(R.id.adView);
+
+        //Step 1
+        MobileAds.initialize(this);
+
+        //Step 2
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        //Step 3
+        adView.loadAd(adRequest);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
